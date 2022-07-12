20458d705f63a80e707a78107d908a52d932ae06
teddybull <teddybull@proton.me>
2022-07-12 01:23:04 +0100

    filtering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Img from "next/image";
 import Head from "next/head";
 
 import daos from "../data/daos";
 import DaoCard from "../components/DaoCard";
+import useAutoFocus from "../hooks/useAutoFocus";
 
 const Home: NextPage = () => {
+  const searchInputRef = useAutoFocus();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredDaos = searchQuery
+    ? daos.filter((dao) =>
+        dao.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : daos;
+
   return (
     <>
       <Head>
@@ -24,6 +35,19 @@ const Home: NextPage = () => {
         <p className="text-center text-gray-500 text-xl mt-4">
           quicklinks for DAO governance
         </p>
+
+        <input
+          autoFocus
+          ref={searchInputRef}
+          placeholder="search for a dao"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="max-w-sm block mx-auto bg-slate-200 p-2 pl-12 mt-8 rounded-full border-2 border-transparent focus:border-slate-500 transition-all outline-none text-slate-700"
+          style={{
+            backgroundImage: "url(/search.svg)",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "12px center",
+          }}
+        />
       </div>
 
       <div
@@ -32,7 +56,7 @@ const Home: NextPage = () => {
           gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
         }}
       >
-        {daos.map((dao) => (
+        {filteredDaos.map((dao) => (
           <DaoCard key={dao.name} dao={dao} />
         ))}
       </div>
