4d58c2aa9c1266abeb59fdd40ca440a9e0eb6ac2
teddybull <teddybull@proton.me>
2022-07-12 01:25:59 +0100

    fix export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+    loader: "custom",
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
