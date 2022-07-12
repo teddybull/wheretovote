d5c192af06cc92c3a79b6a1377f70fb36765e5fa
teddybull <teddybull@proton.me>
2022-07-12 01:23:04 +0100

    add logo and slogan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
+import Img from "next/image";
 import Head from "next/head";
+
 import daos from "../data/daos";
 import DaoCard from "../components/DaoCard";
 
@@ -8,9 +10,21 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>daogov</title>
-        <meta name="description" content="Find DAO governance pages" />
+        <meta
+          name="description"
+          content="Quicklinks for dao governance pages"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className="flex flex-col mt-10 align-center">
+        <h1 className="hidden">Where to vote?</h1>
+        <Img src="/logo.svg" width="434px" height="55px" />
+
+        <p className="text-center text-gray-500 text-xl mt-4">
+          quicklinks for DAO governance
+        </p>
+      </div>
 
       <div
         className="p-8 grid gap-5"
