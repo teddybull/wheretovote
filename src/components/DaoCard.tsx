cd3e8b73a9d50540a11d9727b4c770d158762d11
teddybull <teddybull@proton.me>
2022-07-12 01:23:04 +0100

    make link into button

--- src/components/DaoCard.tsx
+++ src/components/DaoCard.tsx
@@ -8,7 +8,7 @@ const DaoCard = ({ dao }: { dao: Dao }) => {
   const { name, logoUrl, discussUrl, voteUrl } = dao;
 
   return (
-    <div className="bg-white shadow rounded-md p-5 flex flex-col items-center">
+    <div className="bg-white shadow rounded-md p-5 pb-4 flex flex-col items-center">
       <div className="flex-grow flex items-center mt-2">
         {logoUrl ? (
           <Img
@@ -24,7 +24,7 @@ const DaoCard = ({ dao }: { dao: Dao }) => {
 
       <p className="text-gray-900 mt-4 mb-5">{name}</p>
 
-      <div className="flex justify-between gap-3">
+      <div className="flex justify-between gap-2">
         {discussUrl && <Link href={discussUrl}>Discuss</Link>}
         {voteUrl && <Link href={voteUrl}>Vote</Link>}
       </div>
@@ -35,7 +35,7 @@ const DaoCard = ({ dao }: { dao: Dao }) => {
 const Link = ({ href, children }: { children: ReactNode; href: string }) => {
   return (
     <a
-      className="text-blue-600 hover:text-blue-800"
+      className="text-blue-600 hover:text-blue-800 hover:bg-blue-50 py-2 px-3 rounded-md"
       href={href}
       target="_blank"
       rel="noreferrer"
